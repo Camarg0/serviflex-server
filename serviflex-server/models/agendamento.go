e1d5a74da65f030783904478c9991dd1425ba1fe
Bruno Cau de Brito <66956554+brun-brito@users.noreply.github.com>
2025-09-08 12:01:11 -0300

    Add JSON tags to Agendamento struct fields
    Added JSON tags to Agendamento struct fields for serialization.

--- serviflex-server/models/agendamento.go
+++ serviflex-server/models/agendamento.go
@@ -3,10 +3,10 @@ package models
 import "time"
 
 type Agendamento struct {
-	ID                string    `firestore:"id"`
-	ClienteID         string    `firestore:"clienteId"`
-	ProfissionalID    string    `firestore:"profissionalId"`
-	EstabelecimentoID string    `firestore:"estabelecimentoId"`
-	Procedimento      string    `firestore:"procedimento"` // nome do procedimento
-	DataHora          time.Time `firestore:"dataHora"`
+    ID                string    `firestore:"id" json:"id"`
+    ClienteID         string    `firestore:"clienteId" json:"cliente_id"`
+    ProfissionalID    string    `firestore:"profissionalId" json:"profissional_id"`
+    EstabelecimentoID string    `firestore:"estabelecimentoId" json:"estabelecimento_id"`
+    Procedimento      string    `firestore:"procedimento" json:"procedimento"`
+    DataHora          time.Time `firestore:"dataHora" json:"data_hora"`
 }
